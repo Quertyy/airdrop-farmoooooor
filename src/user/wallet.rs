8eca308544c60c9c64f8538816bcc2fba26fbc21
Quertyy <qwertyense@protonmail.com>
2023-07-17 21:15:37 +0200

    feat: wallet generation

--- src/user/wallet.rs
+++ src/user/wallet.rs
@@ -1,5 +1,9 @@
-use ethers::prelude::{k256::ecdsa::SigningKey, *};
+use ethers::{prelude::{k256::ecdsa::SigningKey, *},
+    core::rand,
+    signers::{coins_bip39::English, MnemonicBuilder}
+};
 use crate::utils::dotenv::setup_signer;
+use eyre::Result;
 
 pub struct WalletInfo {
     pub signer: SignerMiddleware<Provider<Http>, Wallet<SigningKey>>,
@@ -12,9 +16,7 @@ pub struct WalletInfo {
 
 impl WalletInfo {
     pub async fn from_pkey(pkey: LocalWallet, provider: Provider<Http>) -> Self {
-        let address = pkey.address();
-        let signer = setup_signer(provider.clone(), pkey).await;
-        let nonce = provider.get_transaction_count(address, None).await.unwrap();
+        let (address, nonce, signer) = Self::get_wallet(pkey, provider).await;
         Self {
             signer,
             address,
@@ -23,5 +25,51 @@ impl WalletInfo {
             transactions_value_amount: U256::zero(),
             erc20_tokens: vec![],
         }
+    }
+
+    pub async fn from_mnemonic(phrase: &str, provider: Provider<Http>) -> Result<Self> {
+        let index = 0u32;
+        let wallet = MnemonicBuilder::<English>::default()
+            .phrase(phrase)
+            .index(index)?
+            .build()?;
+
+        let (address, nonce, signer) = Self::get_wallet(wallet, provider).await;
+        Ok(Self {
+            signer,
+            address,
+            nonce,
+            bridged_amount: U256::zero(),
+            transactions_value_amount: U256::zero(),
+            erc20_tokens: vec![],
+        })
+    }
+
+    pub async fn generate_wallet(provider: Provider<Http>) -> Result<Self> {
+        let mut rng = rand::thread_rng();
+        let wallet = MnemonicBuilder::<English>::default()
+            .word_count(24)
+            .derivation_path("m/44'/60'/0'/2/1")?
+            .build_random(&mut rng)?;
+        
+        let (address, nonce, signer) = Self::get_wallet(wallet, provider).await;
+        Ok(Self {
+            signer,
+            address,
+            nonce,
+            bridged_amount: U256::zero(),
+            transactions_value_amount: U256::zero(),
+            erc20_tokens: vec![],
+        })
+    }
+
+    async fn get_wallet(
+        wallet: LocalWallet, 
+        provider: Provider<Http>
+    ) -> (Address, U256, SignerMiddleware<Provider<Http>, Wallet<SigningKey>>) {
+        let signer = setup_signer(provider.clone(), wallet).await;
+        let address = signer.address();
+        let nonce = provider.get_transaction_count(address, None).await.unwrap();
+        (address, nonce, signer)
     }
 }
